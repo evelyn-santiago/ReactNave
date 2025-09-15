52e2fe0418dc63e5e5c5128299d4003606656f6e
evelyn-santiago <stgoevelyn24@gmail.com>
2025-09-14 19:49:39 -0600

    2.-Haciendo uso de la etiqueta image

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola mundo</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/847/847969.png' }} style={{ width: 150, height: 150, borderRadius:50 }} />
+      <Text style={styles.h2}>Bienvenido </Text>
+      <Text style={styles.h2}>a la clase de párrafos</Text>
+      <Text style={styles.h3}>de la materia de Desarrollo de aplicaciones móviles</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // ocupa toda la pantalla
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#74bad8ff',
+    padding: 20,
+  },
+  h2: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  h3: {
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 22,
   },
 });
