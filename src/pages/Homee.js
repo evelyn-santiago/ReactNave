3c5673af17340094d61ed795b03b47c124f96bf0
evelyn-santiago <stgoevelyn24@gmail.com>
2025-09-14 20:34:19 -0600

    3.-Creando estructura de carpetas y componentes

--- src/pages/Homee.js
+++ src/pages/Homee.js
@@ -5,8 +5,8 @@ function Homee(){
     <SafeAreaView style={styles.container}>
       <Image style={{width:150, height:150, borderRadius:5}} source={{uri: 'https://cdn-icons-png.flaticon.com/512/847/847969.png'}}/>
       <Text style={styles.h3}> Bienvenidos</Text>
-      <Text style={styles.h3}> a la clase de parrafos</Text>
-      <Text style={styles.h3}> de materia de Desarrollo de aplicaciones moviles</Text>
+      <Text style={styles.h2}> a la clase </Text>
+      <Text style={styles.h2}> de materia de Desarrollo de aplicaciones moviles</Text>
     </SafeAreaView>
   );
 }
@@ -15,13 +15,19 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#70bcdfff',
+    alignItems: 'center' ,
+    backgroundColor: '#5c8ea5ff',
   },
   h1:{
     fontSize:20, fontWeight:'bold', marginTop:12,
-  }, h2: {
+  }, 
+  h2: {
     fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 22,
+  },
+  h3: {
+    fontSize: 22,
     fontWeight: '500',
     lineHeight: 22,
   },
