b9e0eb07376868dd55804decc5250b31e47d8d82
evelyn-santiago <stgoevelyn24@gmail.com>
2025-09-17 09:53:11 -0600

    25.-Creando AppBar

--- src/pages/Lista-Alumnos/ListaAlumnos.js
+++ src/pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { View, StyleSheet, Platform, StatusBar,Text } from 'react-native';
+import { View, StyleSheet, Platform, StatusBar, Text, SafeAreaView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const ListaAlumnos = () => {
   return (
-    <View style={style.mainS}>
-      <Text>Holaaaaaa</Text>
-    </View>
+    <SafeAreaView style={style.mainS}>
+      {/* Inicio de AppBar */}
+      <View style={style.appBar}>
+        <Icon name="arrow-left" size={20} color={'#000'} />
+        <Text style={style.appBarTitle}>Lista de Usuarios</Text>
+        <View style={{ width: 20 }}></View>
+      </View>
+      {/* Cierre de AppBar */}
+    </SafeAreaView>
   );
 };
 
 const style = StyleSheet.create({
   mainS: {
     flex: 1,
-    backgroundColor: '#4367ff',
+    backgroundColor: '#bac5f8ff',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    padding: 16
+  }, 
+  appBar: {
+    height: 50,
+    width: '100%',
+    backgroundColor: '#e88aff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16
+  },
+  appBarTitle: {
+    fontWeight: 'bold',
+    fontSize: 16
   }
 });
 
 export default ListaAlumnos;
+
