f134f72a8cfbb33b520c6ecf7212045390f11743
evelyn-santiago <stgoevelyn24@gmail.com>
2025-09-17 10:00:40 -0600

    26.-Creando Lista Alumnos - Card

--- src/pages/Lista-Alumnos/ListaAlumnos.js
+++ src/pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { View, StyleSheet, Platform, StatusBar, Text, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Platform, StatusBar, Text, SafeAreaView, ScrollView, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const ListaAlumnos = () => {
   return (
     <SafeAreaView style={style.mainS}>
-      {/* Inicio de AppBar */}
-      <View style={style.appBar}>
-        <Icon name="arrow-left" size={20} color={'#000'} />
-        <Text style={style.appBarTitle}>Lista de Usuarios</Text>
-        <View style={{ width: 20 }}></View>
-      </View>
-      {/* Cierre de AppBar */}
+      <View style={style.main}>
+  {/* Inicio de AppBar */}
+  <View style={style.appBar}>
+    <Icon name="arrow-left" size={20} color={'#000'} />
+    <Text style={style.appBarTitle}>Lista de Usuarios</Text>
+    <View style={{ width: 20 }}></View>
+  </View>
+  {/* Cierre de AppBar */}
+
+    {/* Inicio de contenido (lista de alumnos) */}
+    <ScrollView>
+        <Text style={style.sectionTitle}>Lista de Alumnos</Text>
+        <View style={style.card}>
+        <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/194/194938.png' }} style={style.avatar} />
+        <View style={style.cardInfo}>
+           <Text style={style.name}>Evelyn Santiago</Text>
+            <Text style={style.userDetails}>Ing. en Sistemas Computacionales</Text>
+        </View>
+        </View>
+    </ScrollView>
+    {/* Cierre de contenido (lista de alumnos) */}
+</View>
     </SafeAreaView>
   );
 };
